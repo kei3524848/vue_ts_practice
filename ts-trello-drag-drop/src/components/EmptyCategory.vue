d539b8e3117a0b24715ff30e2fa24041d4ba66f0
kei3524848 <kei3524848@gmail.com>
2022-07-10 16:14:45 +0900

    Merge branch 'develop' of https://github.com/kei3524848/vue_ts_practice into develop

---
+++ ts-trello-drag-drop/src/components/EmptyCategory.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import type { Category } from "../types";
+
+defineProps<{
+  category: Category;
+}>();
+</script>
+
+<template>
+  <div class="bg-gray-200 m-2 p-2 text-sm">
+    <span class="font-bold"> {{ category.name }}</span>
+  </div>
+</template>
